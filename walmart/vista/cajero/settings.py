571297c864d66e4bbf5faa74d374f642d5d815fe
Sigfrido <sigfrido_o@ardilluda.com.mx>
2021-10-25 16:22:56 -0500

    Conflictos resueltos

--- walmart/vista/cajero/settings.py
+++ walmart/vista/cajero/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6g1avf)1=poiwc6f6#c*onk9a_r9478zm*^j_m#vlvhof$$ep*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
